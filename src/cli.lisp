a10790a5b1a3ffe7e5c0cbd71ab36106fef3ccaf
Fernando Borretti <fernando@borretti.me>
2022-12-01 20:28:13 +1100

    Parse CLI arguments

--- src/cli.lisp
+++ src/cli.lisp
@@ -24,7 +24,11 @@
   ((name :reader :command-name
          :initarg :name
          :type symbol
-         :documentation "The name of the configuration to apply."))
+         :documentation "The name of the configuration to apply.")
+   (files :reader command-files
+          :initarg :files
+          :type list
+          :documentation "The list of files to load in order."))
   (:documentation "Command to apply a configuration."))
 
 (defclass unapply-command (command)
@@ -55,4 +59,28 @@
 ;;;; Command parsing.
 
 (defun parse-cli (args)
-  "Parse a list of command line arguments.")
+  "Parse a list of command line arguments."
+  (if (null args)
+      ;; Empty list: print help
+      (make-instance 'help-command)
+      ;; At least one argument
+      (let ((first (first args)))
+        (cond ((string= first "get")
+               (make-instance 'get-command))
+              ((string= first "apply")
+               (destructuring-bind (name &rest files)
+                   (rest args)
+                 (make-instance 'apply-command
+                                :name (let ((name (read name)))
+                                        (if (symbolp name)
+                                            name
+                                            (error "Name is not a symbol.")))
+                                :files files)))
+              ((string= first "unapply")
+               (make-instance 'unapply-command))
+              ((string= first "help")
+               (make-instance 'help-command))
+              ((string= first "version")
+               (make-instance' 'version-command))
+              (t
+               (make-instance 'help-command))))))
