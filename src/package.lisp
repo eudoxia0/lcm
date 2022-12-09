254c82eeaa27050cc5674b9d92ef6d2237708ced
Fernando Borretti <fernando@borretti.me>
2022-12-10 10:28:57 +1100

    Export some symbols

--- src/package.lisp
+++ src/package.lisp
@@ -1,4 +1,17 @@
 (in-package :cl-user)
 (defpackage lcm
   (:use :cl)
-  (:export :entrypoint))
+  (:export :configuration
+           :configuration-name
+           :configuration-components
+           :component
+           :component-title
+           :component-applied-p
+           :component-apply
+           :component-unapply
+           :secrets-template
+           :secrets-template-keys
+           :vault
+           :vault-table
+           :get-secret
+           :entrypoint))
