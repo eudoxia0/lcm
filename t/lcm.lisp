c528b77b3239665a362d52de3f0a2a61e379929e
Fernando Borretti <fernando@borretti.me>
2022-12-10 09:27:44 +1100

    Add tests

--- t/lcm.lisp
+++ t/lcm.lisp
@@ -9,6 +9,7 @@
 ;;;; Secrets.
 
 (define-test hash-table-keys-tests
+  :parent lcm
   (let ((table (make-hash-table :test #'equal)))
     (setf (gethash "A" table) 1
           (gethash "B" table) 2
@@ -18,6 +19,13 @@
       (is string= (nth keys 0) "A")
       (is string= (nth keys 0) "B")
       (is string= (nth keys 0) "C"))))
+
+;;;; Command line parsing.
+
+(define-test help-command
+  :parent lcm
+  (let ((cmd (lcm::parse-cli (list "help"))))
+    (of-type cmd 'lcm::help-command)))
 
 ;;;; Interface
 
