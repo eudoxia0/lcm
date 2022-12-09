96923c201a482dbf0c9f6034b83b3ef8827a984e
Fernando Borretti <fernando@borretti.me>
2022-12-10 10:18:10 +1100

    Fix registry

--- src/registry.lisp
+++ src/registry.lisp
@@ -1,6 +1,6 @@
 (in-package :lcm)
 
-(defparameter *config-registry* (make-hash-table :test #'eq))
+(defparameter *config-registry* (make-hash-table :test 'eq))
 
 (defun register-configuration (config)
   (setf (gethash (configuration-name config) *config-registry*) config))
