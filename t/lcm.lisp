6a1c5b28631a50407b49bda38942db5bdcc11f6b
Fernando Borretti <fernando@borretti.me>
2022-12-10 09:46:05 +1100

    Fixes

--- t/lcm.lisp
+++ t/lcm.lisp
@@ -4,64 +4,64 @@
   (:export :run-tests))
 (in-package :lcm-test)
 
-(define-suite lcm)
+(define-test suite)
 
 ;;;; Secrets.
 
 (define-test hash-table-keys-tests
-  :parent lcm
+  :parent suite
   (let ((table (make-hash-table :test #'equal)))
     (setf (gethash "A" table) 1
           (gethash "B" table) 2
           (gethash "C" table) 3)
     (let ((keys (sort (lcm::hash-table-keys table) #'string<)))
       (is = (length keys) 3)
-      (is string= (nth keys 0) "A")
-      (is string= (nth keys 0) "B")
-      (is string= (nth keys 0) "C"))))
+      (is string= (nth 0 keys) "A")
+      (is string= (nth 1 keys) "B")
+      (is string= (nth 2 keys) "C"))))
 
 ;;;; Command line parsing.
 
 (define-test help-command
-  :parent lcm
+  :parent suite
   (let ((cmd (lcm::parse-cli (list "help"))))
-    (of-type cmd 'lcm::help-command)))
+    (of-type 'lcm::help-command cmd)))
 
 (define-test version-command
-  :parent lcm
+  :parent suite
   (let ((cmd (lcm::parse-cli (list "version"))))
-    (of-type cmd 'lcm::version-command)))
+    (of-type 'lcm::version-command cmd)))
 
 (define-test get-command
-  :parent lcm
+  :parent suite
   (let ((cmd (lcm::parse-cli (list "get"))))
-    (of-type cmd 'lcm::get-command)))
+    (of-type 'lcm::get-command cmd)))
 
 (define-test apply-command
-  :parent lcm
+  :parent suite
   (let ((cmd (lcm::parse-cli (list "apply bar foo.lisp"))))
-    (of-type cmd 'lcm::get-command)
-    (is string= (command-name cmd) 'bar)
-    (is equal (command-files cmd) (list "foo.lisp"))
-    (is equal (command-secrets cmd) nil))
+    (of-type 'lcm::get-command cmd)
+    (is eq (lcm::command-name cmd) 'bar)
+    (is equal (lcm::command-files cmd) (list "foo.lisp"))
+    (is equal (lcm::command-secrets cmd) nil))
   (let ((cmd (lcm::parse-cli (list "apply bar foo.lisp --secrets=derp.sexp"))))
-    (of-type cmd 'lcm::get-command)
-    (is string= (command-name cmd) 'bar)
-    (is equal (command-files cmd) (list "foo.lisp"))
-    (is equal (command-secrets cmd) "derp.sexp")))
+    (of-type 'lcm::get-command cmd)
+    (is eq (lcm::command-name cmd) 'bar)
+    (is equal (lcm::command-files cmd) (list "foo.lisp"))
+    (is equal (lcm::command-secrets cmd) "derp.sexp")))
 
 (define-test unapply-command
-  :parent lcm
+  :parent suite
   (let ((cmd (lcm::parse-cli (list "unapply foo.lisp"))))
-    (of-type cmd 'lcm::get-command)
-    (is equal (command-files cmd) (list "foo.lisp"))
-    (is equal (command-secrets cmd) nil))
+    (of-type 'lcm::get-command cmd)
+    (is equal (lcm::command-files cmd) (list "foo.lisp"))
+    (is equal (lcm::command-secrets cmd) nil))
   (let ((cmd (lcm::parse-cli (list "unapply foo.lisp --secrets=derp.sexp"))))
-    (of-type cmd 'lcm::get-command)
-    (is equal (command-files cmd) (list "foo.lisp"))
-    (is equal (command-secrets cmd) "derp.sexp")))
+    (of-type 'lcm::get-command cmd)
+    (is equal (lcm::command-files cmd) (list "foo.lisp"))
+    (is equal (lcm::command-secrets cmd) "derp.sexp")))
 
 ;;;; Interface
 
 (defun run-tests ()
-  (test 'lcm))
+  (test 'suite))
