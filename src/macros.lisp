0d5feaee31fd5af68f43e0ffdc9f9f2e04f52fa8
Fernando Borretti <fernando@borretti.me>
2022-12-10 11:00:50 +1100

    add macros.lisp

---
+++ src/macros.lisp
@@ -0,0 +1,14 @@
+(in-package :lcm)
+
+(defmacro defcomponent (name superclass-list slots &key documentation appliedp apply unapply)
+  "Shorthand for defining a component."
+  `(progn
+     (defclass ,name ,superclass-list
+       ,slots
+       (:documentation ,documentation))
+
+     (defmethod component-applied-p ,@appliedp)
+
+     (defmethod component-apply ,@apply)
+
+     (defmethod component-nuapply ,@unapply)))
